a79942b0ac9275921031d875042bee2bd4a65f1d
Christoph Borgolte <christoph.borgolte@vaillant-group.com>
2021-09-02 15:48:59 +0200

    Setup App.tsx for logging to instana
    + contains some debug code to identify XMLHttpRequest and fetch issues.

--- App.tsx
+++ App.tsx
@@ -9,6 +9,10 @@
  */
 
 import React from 'react';
+import Instana from '@instana/react-native-agent';
+import axios, {AxiosRequestConfig} from 'axios';
+import fetchAdapter from '@vespaiach/axios-fetch-adapter';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -31,9 +35,101 @@ const Section: React.FC<{
   title: string;
 }> = ({children, title}) => {
   const isDarkMode = useColorScheme() === 'dark';
+
+  React.useEffect(() => {
+    const INSTANA_APP_KEY = 'xxxxxxxxxxxxxxxxxxxxxx';
+    const INSTANA_REPORTING_URL = 'https://eum-blue-saas.instana.io/mobile';
+    Instana.setup(INSTANA_APP_KEY, INSTANA_REPORTING_URL);
+    Instana.setIgnoreURLsByRegex(['http://localhost:8081.*']);
+  }, []);
+
+  const httpClient = axios.create({adapter: fetchAdapter});
+  // const httpClient = fetch; // this works!
+
+  const callAxios = async () => {
+    try {
+      console.log('callAxios');
+      let response = await httpClient('https://reactnative.dev/movies.json');
+      let json = await response.data;
+      console.log('callAxios', 'success', response);
+    } catch (error) {
+      console.warn('callAxios', error);
+    }
+  };
+
+  const callFetch = async () => {
+    try {
+      console.log('callFetch');
+      let stageOne = await fetch('https://reactnative.dev/movies.json');
+      const response = {
+        ok: stageOne.ok,
+        status: stageOne.status,
+        statusText: stageOne.statusText,
+        headers: new Headers(stageOne.headers), // Make a copy of headers
+      };
+      let json = await stageOne.json();
+      console.log('callFetch', 'success', response, json);
+    } catch (error) {
+      console.warn('callFetch', error);
+    }
+  };
+
+  const onRunXMLHttpRequest = async () => {
+    console.log('onRunXMLHttpRequest');
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = e => {
+      if (request.readyState !== 4) {
+        return;
+      }
+      console.log('*** onRunXMLHttpRequest', {
+        status: request.status,
+        statusText: request.statusText,
+        responseText: request.responseText,
+        e,
+        request,
+      });
+
+      if (request.status === 200) {
+        console.log('onRunXMLHttpRequest', 'success', request.responseText);
+      } else {
+        console.log('onRunXMLHttpRequest', 'error');
+      }
+    };
+
+    request.open('GET', 'https://reactnative.dev/movies.json');
+    request.send();
+  };
+
+  const onClick = () => {
+    // callAxios();
+    // callFetch();
+    onRunXMLHttpRequest();
+
+    //// const config: AxiosRequestConfig = {
+    ////   method: 'POST',
+    ////   data: 'username=test%40vaillant.de&password=sekret&grant_type=password_public',
+    ////   headers: {
+    ////     accept: 'application/json, text/plain, */*',
+    ////     'content-type': 'application/x-www-form-urlencoded',
+    ////   },
+    //// };
+    //// console.log('click', config);
+    //// httpClient('https://httpbin.org/post', config)
+    ////   .then(response =>
+    ////     console.log('RESP: ', {
+    ////       status: response.status,
+    ////       statusText: response.statusText,
+    ////       response: response.json,
+    ////     }),
+    ////   )
+    ////   .catch(reason => console.log('CATCH:', reason))
+    ////   .finally(() => console.log('FINALLY'));
+  };
+
   return (
     <View style={styles.sectionContainer}>
       <Text
+        onPress={onClick}
         style={[
           styles.sectionTitle,
           {
